ad46db8c0a3ebbcff5e5ff465f1bbafd74583db7
tanutsorn2522 <tanutsorn@gmail.com>
2022-02-17 14:45:23 +0700

    Update Product

--- lib/body/show_product_seller.dart
+++ lib/body/show_product_seller.dart
@@ -5,7 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shops/models/product_model.dart';
 import 'package:shops/utility/my_constant.dart';
+import 'package:shops/widgets/show_image.dart';
 import 'package:shops/widgets/show_progress.dart';
+import 'package:shops/widgets/show_title.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ShowProductSeller extends StatefulWidget {
   ShowProductSeller({Key? key}) : super(key: key);
@@ -16,6 +19,8 @@ class ShowProductSeller extends StatefulWidget {
 
 class _ShowProductSellerState extends State<ShowProductSeller> {
   bool load = true;
+  bool? haveData;
+  List<ProductModel> productModels = [];
 
   @override
   void initState() {
@@ -30,13 +35,24 @@ class _ShowProductSellerState extends State<ShowProductSeller> {
     String apiGetProductWhereIdSeller =
         '${MyConstant.domain}/shops/getProductWhereSeller.php?isAdd=true&idSeller=$id';
     await Dio().get(apiGetProductWhereIdSeller).then((value) {
-      for (var item in json.decode(value.data)) {
-        ProductModel model = ProductModel.fromMap(item);
-        print('name Product ==>> ${model.name}');
-
+      if (value.toString() == 'null') {
+        // No Data
         setState(() {
           load = false;
+          haveData = false;
         });
+      } else {
+        // มีข้อมูล
+        for (var item in json.decode(value.data)) {
+          ProductModel model = ProductModel.fromMap(item);
+          print('name Product ==>> ${model.name}');
+
+          setState(() {
+            load = false;
+            haveData = true;
+            productModels.add(model);
+          });
+        }
       }
     });
   }
@@ -44,12 +60,148 @@ class _ShowProductSellerState extends State<ShowProductSeller> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: load ? ShowProgress() : Text('Load Finish'),
+      body: load
+          ? ShowProgress()
+          : haveData!
+              ? LayoutBuilder(
+                  builder: (context, constraints) => buildListView(constraints),
+                )
+              : Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ShowTitle(
+                          title: 'ไม่มีสินค้า',
+                          textStyle: MyConstant().h1Style()),
+                      ShowTitle(
+                          title: 'กรุณาเพิ่มสินค้า',
+                          textStyle: MyConstant().h2Style()),
+                    ],
+                  ),
+                ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: MyConstant.primary,
         onPressed: () =>
             Navigator.pushNamed(context, MyConstant.routeAddProduce),
         child: Text('Add'),
+      ),
+    );
+  }
+
+  String createUrl(String string) {
+    String result = string.substring(1, string.length - 1);
+    List<String> strings = result.split(',');
+    String url = '${MyConstant.domain}/shops/img/products/${strings[0]}';
+    return url;
+  }
+
+  ListView buildListView(BoxConstraints constraints) {
+    return ListView.builder(
+      itemCount: productModels.length,
+      itemBuilder: (context, index) => Card(
+        child: Row(
+          children: [
+            Container(
+              padding: EdgeInsets.all(4),
+              width: constraints.maxWidth * 0.5 - 4,
+              height: constraints.maxWidth * 0.5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ShowTitle(
+                      title: productModels[index].name,
+                      textStyle: MyConstant().h2Style()),
+                  Container(
+                    width: constraints.maxWidth * 0.5,
+                    height: constraints.maxWidth * 0.4,
+                    child: CachedNetworkImage(
+                      fit: BoxFit.cover,
+                      imageUrl: createUrl(productModels[index].images),
+                      placeholder: (context, url) => ShowProgress(),
+                      errorWidget: (context, url, error) =>
+                          ShowImage(path: MyConstant.imageicon1),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.all(4),
+              width: constraints.maxWidth * 0.5 - 4,
+              height: constraints.maxWidth * 0.4,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ShowTitle(
+                      title: 'ราคา ${productModels[index].price} บาท',
+                      textStyle: MyConstant().h2Style()),
+                  ShowTitle(
+                      title: productModels[index].detail,
+                      textStyle: MyConstant().h3Style()),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.edit_outlined,
+                          size: 24,
+                          color: MyConstant.dark,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          print('## คุณคลิก Delete for index = $index');
+                          confirmDialogDelete(productModels[index]);
+                        },
+                        icon: Icon(
+                          Icons.delete_outlined,
+                          size: 24,
+                          color: MyConstant.dark,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Null> confirmDialogDelete(ProductModel productModel) async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: ListTile(
+          leading: CachedNetworkImage(
+            imageUrl: createUrl(productModel.images),
+            placeholder: (context, url) => ShowProgress(),
+          ),
+          title: ShowTitle(
+            title: 'Delete ${productModel.name} ?',
+            textStyle: MyConstant().h2Style(),
+          ),
+          subtitle: ShowTitle(
+            title: productModel.detail,
+            textStyle: MyConstant().h3Style(),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Delete'),
+          ),
+        ],
       ),
     );
   }
