50cf7ad346f8ea16b159c03ae981ce0b38bef113
tanutsorn2522 <tanutsorn@gmail.com>
2022-02-18 21:48:18 +0700

    Update Edit Product

--- lib/states/edit_product.dart
+++ lib/states/edit_product.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shops/models/product_model.dart';
 import 'package:shops/utility/my_constant.dart';
@@ -21,6 +22,8 @@ class _EditProductState extends State<EditProduct> {
   TextEditingController detailController = TextEditingController();
 
   List<String> pathImages = [];
+
+  final formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -51,23 +54,39 @@ class _EditProductState extends State<EditProduct> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: [
+          IconButton(
+            onPressed: () => processEdit(),
+            icon: Icon(Icons.edit),
+            tooltip: 'Edit Product',
+          ),
+        ],
       ),
       body: LayoutBuilder(
         builder: (context, constraints) => Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                buildTitle('General'),
-                buidName(constraints),
-                buidPrice(constraints),
-                buidDetail(constraints),
-                buildTitle('Images Product'),
-                buildImage(constraints, 0),
-                buildImage(constraints, 1),
-                buildImage(constraints, 2),
-                buildImage(constraints, 3),
-              ],
+            child: GestureDetector(
+              onTap: () =>
+                  FocusScope.of(context).requestFocus(FocusScopeNode()),
+              behavior: HitTestBehavior.opaque,
+              child: Form(
+                key: formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    buildTitle('General'),
+                    buidName(constraints),
+                    buidPrice(constraints),
+                    buidDetail(constraints),
+                    buildTitle('Images Product'),
+                    buildImage(constraints, 0),
+                    buildImage(constraints, 1),
+                    buildImage(constraints, 2),
+                    buildImage(constraints, 3),
+                    buildEditProduct(constraints)
+                  ],
+                ),
+              ),
             ),
           ),
         ),
@@ -75,8 +94,24 @@ class _EditProductState extends State<EditProduct> {
     );
   }
 
+  Container buildEditProduct(BoxConstraints constraints) {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      width: constraints.maxWidth,
+      child: ElevatedButton.icon(
+          onPressed: () => processEdit(),
+          icon: Icon(Icons.edit),
+          label: Text('Edit Produce')),
+    );
+  }
+
   Container buildImage(BoxConstraints constraints, int index) {
     return Container(
+      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade400),
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
@@ -85,6 +120,7 @@ class _EditProductState extends State<EditProduct> {
             icon: Icon(Icons.add_a_photo),
           ),
           Container(
+            padding: EdgeInsets.symmetric(vertical: 8),
             width: constraints.maxWidth * 0.5,
             child: CachedNetworkImage(
               imageUrl:
@@ -109,6 +145,13 @@ class _EditProductState extends State<EditProduct> {
           margin: const EdgeInsets.only(top: 16.0),
           width: constraints.maxWidth * 0.75,
           child: TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill Name is Blank';
+              } else {
+                return null;
+              }
+            },
             controller: nameController,
             decoration: InputDecoration(
               labelText: 'ชื่อสินค้า',
@@ -128,6 +171,14 @@ class _EditProductState extends State<EditProduct> {
           margin: EdgeInsets.symmetric(vertical: 16),
           width: constraints.maxWidth * 0.75,
           child: TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill Price';
+              } else {
+                return null;
+              }
+            },
+            keyboardType: TextInputType.number,
             controller: priceController,
             decoration: InputDecoration(
               labelText: 'ราคาสินค้า',
@@ -146,6 +197,13 @@ class _EditProductState extends State<EditProduct> {
         Container(
           width: constraints.maxWidth * 0.75,
           child: TextFormField(
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please Fill Detail';
+              } else {
+                return null;
+              }
+            },
             maxLines: 3,
             controller: detailController,
             decoration: InputDecoration(
@@ -167,5 +225,14 @@ class _EditProductState extends State<EditProduct> {
         ),
       ],
     );
+  }
+
+  processEdit() {
+    if (formKey.currentState!.validate()) {
+      String name = nameController.text;
+      String price = priceController.text;
+      String detail = detailController.text;
+      print('## name = $name, price = $price, detail = $detail');
+    }
   }
 }
