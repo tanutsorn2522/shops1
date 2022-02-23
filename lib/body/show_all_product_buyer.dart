90faf89f5b9b91f375a4edbcde4140ed0280d3b3
tanutsorn2522 <tanutsorn@gmail.com>
2022-02-23 16:18:59 +0700

    Update Buyer

--- lib/body/show_all_product_buyer.dart
+++ lib/body/show_all_product_buyer.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:shops/models/user_model.dart';
+import 'package:shops/utility/my_constant.dart';
 
 class ShowAllProductBuyer extends StatefulWidget {
   const ShowAllProductBuyer({Key? key}) : super(key: key);
@@ -8,6 +13,24 @@ class ShowAllProductBuyer extends StatefulWidget {
 }
 
 class _ShowAllProductBuyerState extends State<ShowAllProductBuyer> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    readApiAllShop();
+  }
+
+  Future<Null> readApiAllShop() async {
+    String urlAPI = '${MyConstant.domain}/shops/getUserWhereSeller.php';
+    await Dio().get(urlAPI).then((value) {
+      var result = json.decode(value.data);
+      for (var item in result) {
+        UserModel model = UserModel.fromMap(item);
+        print('name ==>> ${model.name}');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
