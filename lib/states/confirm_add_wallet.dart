73624040816f15b3858574a81f5fd729929871f4
tanutsorn2522 <tanutsorn@gmail.com>
2022-03-21 11:43:47 +0700

    update Confirm

--- lib/states/confirm_add_wallet.dart
+++ lib/states/confirm_add_wallet.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:shops/utility/my_constant.dart';
+import 'package:shops/widgets/show_title.dart';
 
 class ConfirmAddWallet extends StatefulWidget {
   const ConfirmAddWallet({Key? key}) : super(key: key);
@@ -11,6 +12,23 @@ class ConfirmAddWallet extends StatefulWidget {
 }
 
 class _ConfirmAddWalletState extends State<ConfirmAddWallet> {
+  late String dateTimeStr;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    findCurrentTime();
+  }
+
+  void findCurrentTime() {
+    DateTime dateTime = DateTime.now();
+
+    dateTimeStr = dateTime.toString();
+    print('dateTimeStr ==>> $dateTimeStr');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +41,19 @@ class _ConfirmAddWalletState extends State<ConfirmAddWallet> {
               ? Icon(Icons.arrow_back_ios)
               : Icon(Icons.arrow_back),
         ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ShowTitle(
+            title: 'Current Date Pay',
+            textStyle: MyConstant().h1Style(),
+          ),
+          ShowTitle(
+            title: 'dd/mm/yy HH:mm',
+            textStyle: MyConstant().h2Style(),
+          )
+        ],
       ),
     );
   }
